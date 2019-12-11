6893b51798157fbad7a8520ff8c0d9f0e5b34f3f
collagePC <ateruimashin2go@gmail.com>
2019-12-11 15:39:53 +0900

    even bug

--- chacha.cpp
+++ chacha.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 //出典:https://boringssl.googlesource.com/boringssl/+/master/crypto/chacha/chacha.c
-#define ROTL(a,b) ((a) << (b)) | ((a) >> (32-(b) ))
+#define ROTL(a,b) (((a) << (b)) | ((a) >> (32 - (b))))
 #define QR(a, b, c, d) (			\
 	a += b,  d ^= a,  d = ROTL(d,16),	\
 	c += d,  b ^= c,  b = ROTL(b,12),	\
@@ -47,7 +47,7 @@ array<uint32_t,8> make_array_nonce(string s){
 }
 
 
-void chacha(string key, string nonce) {
+void chacha(string key, string nonce, string cipher) {
 
 // //debug
 // cout<<"key="<<key<<"&nonce="<<nonce<<endl;
@@ -78,7 +78,7 @@ void chacha(string key, string nonce) {
 
 
   //debug
-  cout<<"key作成終了"<<endl;
+  // cout<<"key作成終了"<<endl;
 
   //上で作成したkey,block_count,nonceをInital Stateに代入。
   //この時、Initial Stateは64要素ある。
@@ -117,69 +117,93 @@ void chacha(string key, string nonce) {
   }
 
   //debug
-  cout<<"4*4のInital State完成"<<endl;
+  // cout<<"4*4のInital State完成"<<endl;
 
   //QR前のx[]をコピーする。
   uint32_t cp[16]={};
 
 	//debug
-	cout<<"cp作成"<<endl;
+	// cout<<"cp作成"<<endl;
 
   for(int i = 0; i < 16; i++){
 		cp[i] = x[i];
 	}
 
-  //debug
-  cout<<"コピー完了"<<endl;
-
   //x[]をQRに通す
   for(int i = 0; i < ROUNDS; i += 2){
-    // Odd round
-		QR(x[ 0], x[ 4], x[ 8], x[12]);	// column 1
-		QR(x[ 5], x[ 9], x[13], x[ 1]);	// column 2
-		QR(x[10], x[14], x[ 2], x[ 6]);	// column 3
-		QR(x[15], x[ 3], x[ 7], x[11]);	// column 4
+		// Odd round
+			QR(x[0], x[4], x[ 8], x[12]); // column 0
+			QR(x[1], x[5], x[ 9], x[13]); // column 1
+			QR(x[2], x[6], x[10], x[14]); // column 2
+			QR(x[3], x[7], x[11], x[15]); // column 3
 		// Even round
-		QR(x[ 0], x[ 1], x[ 2], x[ 3]);	// row 1
-		QR(x[ 5], x[ 6], x[ 7], x[ 4]);	// row 2
-		QR(x[10], x[11], x[ 8], x[ 9]);	// row 3
-		QR(x[15], x[12], x[13], x[14]);	// row 4
+		QR(x[0], x[5], x[10], x[15]); // diagonal 1 (main diagonal)
+		QR(x[1], x[6], x[11], x[12]); // diagonal 2
+		QR(x[2], x[7], x[ 8], x[13]); // diagonal 3
+		QR(x[3], x[4], x[ 9], x[14]); // diagonal 4
   }
 
   //debug
-  cout<<"QR終了"<<endl;
+  // cout<<"QR終了"<<endl;
 
   //QR前x[]とQR後x[]を2^32を法とする加算を行う。
   for(int i = 0; i < 16; i++){
     x[i] = plus32(x[i], cp[i]);
   }
 
+	for(int i=0;i<16;i++){
+		cout<<i<<"="<<x[i]<<endl;
+	}
+
   //debug
-  cout<<"2^32加算終了"<<endl;
+  // cout<<"2^32加算終了"<<endl;
 
   //リトルエンディアンを逆変換する
   uint32_t result[64]={};
-  for(int i = 0; i < 64; i ++){
-    result[i] =  (x[i] & 0xff);
-    result[i] = ((x[i] >> 8) & 0xff);
-    result[i] = ((x[i] >>16) & 0xff);
-    result[i] = ((x[i] >> 24) & 0xff);
+  for(int i = 0; i < 64; i +=4){
+    result[i]		  = (x[i] & 0xff);
+		cout<<i<<"="<<result[i]<<endl;
+
+    result[i+1] = ((x[i] >> 8) & 0xff);
+		cout<<i+1<<"="<<result[i]<<endl;
+
+    result[i+2] = ((x[i] >>8) & 0xff);
+		cout<<i+2<<"="<<result[i]<<endl;
+
+    result[i+3] = ((x[i] >> 8) & 0xff);
+		cout<<i+3<<"="<<result[i]<<endl;
   }
 
   //debug
-  cout<<"逆リトルエンディアン終了"<<endl;
+  // cout<<"逆リトルエンディアン終了"<<endl;
 
 //16進数のstring型に変換する。これがkey_steam。
   for(int i = 0; i < 64; i++){
     cout<<hex<<result[i];
+		if((i+1)%8 ==0){
+			cout<<"  ";
+		}
   }
   cout<<endl;
+	//
+	// array<uint32_t,32> c(make_array_key(cipher));
+	// array<uint32_t,32> m={};
+	// for(int i=0;i<c.size();i++){
+	// 	m[i] = c[i] ^ result[i];
+	// 	cout<<"m["<<i<<"]="<<m[i]<<endl;
+	// }
+	//
+	// for(int i=0;i<m.size();i++){
+	// 	cout<<char(m[i]);
+	// }
+	// cout<<endl;
 }
 
 int main(int argc, char const *argv[]) {
-  string key, nonce;
+  string key, nonce,cipher;
 	key = "0000000000000000000000000000000000000000000000000000000000000000";
   nonce = "0000000000000000";
-  chacha(key, nonce);
+	cipher = "6bd00ba222523f58de196fb471eea08d9fff95b5bbe6123dd3a8b9026ac0fa84";
+  chacha(key, nonce, cipher);
   return 0;
 }
