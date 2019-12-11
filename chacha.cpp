894d04d1ed0a8b9459cdd3a032f15a40364ae59a
ateruimashin <ateruimashin2go@gmail.com>
2019-12-12 02:41:32 +0900

    変更点 /chacha関数からstring型が返るようになった /key_generateを作った

--- chacha.cpp
+++ chacha.cpp
@@ -2,11 +2,13 @@
 #include <string>
 #include <array>
 #include <cstring>
-
+#include <vector>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 //出典:https://boringssl.googlesource.com/boringssl/+/master/crypto/chacha/chacha.c
-#define ROTL(a,b) ((a) << (b)) | ((a) >> (32-(b) ))
+#define ROTL(a,b) (((a) << (b)) | ((a) >> (32 - (b))))
 #define QR(a, b, c, d) (			\
 	a += b,  d ^= a,  d = ROTL(d,16),	\
 	c += d,  b ^= c,  b = ROTL(b,12),	\
@@ -18,7 +20,7 @@ uint32_t plus32(uint32_t x,uint32_t y){
   return (x+y) & 0xffffffff;
 }
 
-array<uint32_t, 32> make_array_key(string s){
+array<uint32_t,32> make_array_key(string s){
   array<uint32_t,32> key;
   int count = 0;
   for(int i=0;i<s.size();i+=2){
@@ -46,14 +48,30 @@ array<uint32_t,8> make_array_nonce(string s){
   return key;
 }
 
+string key_generate(int a, int b, int c, int d){
+	string sub_key;
+	int bc[4] = {a, b, c, d};
+	for(int i = 0; i < 4; i++){
+		char c;
+		if(bc[i] < 10){
+			c = bc[i] + '0';
+		}else{
+			if(bc[i] == 10)	c = 'a';
+			if(bc[i] == 11)	c = 'b';
+			if(bc[i] == 12)	c = 'b';
+			if(bc[i] == 13)	c = 'd';
+			if(bc[i] == 14)	c = 'e';
+			if(bc[i] == 15)	c = 'f';
+		}
+		sub_key.push_back(c);
+	}
+	return sub_key;
+}
 
-void chacha(string key, string nonce) {
-
-//debug
-cout<<"key="<<key<<"&nonce="<<nonce<<endl;
+string chacha(string key, string nonce) {
 
   //Initial Stateを作成する。
-  array<uint32_t,32>  in = {101, 120, 112, 97,
+  array<uint32_t,64>  in = {101, 120, 112, 97,
                                             110, 100, 32 , 51,
                                             50, 45, 98 , 121,
                                             116, 101, 32, 107};
@@ -63,100 +81,99 @@ cout<<"key="<<key<<"&nonce="<<nonce<<endl;
   array<uint32_t, 8> block_count = {0, 0, 0, 0, 0, 0, 0, 0};
   array<uint32_t, 8> n(make_array_nonce(nonce));
 
-  //debug
-  cout<<"key作成終了"<<endl;
 
-  //上で作成したkey,block_count,nonceをInital Stateに代入。
-  //この時、Initial Stateは64要素ある。
   for(int i = 16; i < 64; i++){
-		//debug
-		cout<<"i="<<i<<endl;
     if(i >= 16 && i < 48){
       in[i] = k[i - 16];
-      //debug
-      cout<<1<<endl;
     }else if(i >= 48 && i < 56){
       in[i] = block_count[i - 48];
-      //debug
-      cout<<2<<endl;
     }else{
       in[i] = n[i - 56];
-      //debug
-      cout<<3<<endl;
     }
   }
-
-//debug
-cout<<"1*64のInital State完成"<<endl;
 
 /*4*4のInital Stateに変換する。64要素あるInital Stateを4要素ずつ取り出し、リトルエンディアンに変換して4*4行列に代入する。
 この時、4*4行列ではなく、QRの計算のために1*16行列にする。つまり、このあと計算するのはIn[]ではなく、x[]である。
 また、これはリトルエンディアンにしている。*/
   uint32_t x[16]={};
-  for(int i = 0; i < 64; i++){
+  for(int i = 0; i < 64; i+=4){
     x[i / 4] = in[i] | (in[i+1] << 8) | (in[i+2] << 16) | (in[i+3] << 24);
   }
 
-  //debug
-  cout<<"4*4のInital State完成"<<endl;
-
   //QR前のx[]をコピーする。
   uint32_t cp[16]={};
-  memcpy(cp, x, sizeof(x));
 
-  //debug
-  cout<<"コピー完了"<<endl;
+  for(int i = 0; i < 16; i++){
+		cp[i] = x[i];
+	}
 
   //x[]をQRに通す
   for(int i = 0; i < ROUNDS; i += 2){
-    // Odd round
-		QR(x[ 0], x[ 4], x[ 8], x[12]);	// column 1
-		QR(x[ 5], x[ 9], x[13], x[ 1]);	// column 2
-		QR(x[10], x[14], x[ 2], x[ 6]);	// column 3
-		QR(x[15], x[ 3], x[ 7], x[11]);	// column 4
+		// Odd round
+			QR(x[0], x[4], x[ 8], x[12]); // column 0
+			QR(x[1], x[5], x[ 9], x[13]); // column 1
+			QR(x[2], x[6], x[10], x[14]); // column 2
+			QR(x[3], x[7], x[11], x[15]); // column 3
 		// Even round
-		QR(x[ 0], x[ 1], x[ 2], x[ 3]);	// row 1
-		QR(x[ 5], x[ 6], x[ 7], x[ 4]);	// row 2
-		QR(x[10], x[11], x[ 8], x[ 9]);	// row 3
-		QR(x[15], x[12], x[13], x[14]);	// row 4
+		QR(x[0], x[5], x[10], x[15]); // diagonal 1 (main diagonal)
+		QR(x[1], x[6], x[11], x[12]); // diagonal 2
+		QR(x[2], x[7], x[ 8], x[13]); // diagonal 3
+		QR(x[3], x[4], x[ 9], x[14]); // diagonal 4
   }
-
-  //debug
-  cout<<"QR終了"<<endl;
 
   //QR前x[]とQR後x[]を2^32を法とする加算を行う。
   for(int i = 0; i < 16; i++){
     x[i] = plus32(x[i], cp[i]);
   }
 
-  //debug
-  cout<<"2^32加算終了"<<endl;
-
   //リトルエンディアンを逆変換する
   uint32_t result[64]={};
-  for(int i = 0; i < 64; i ++){
-    result[i] =  (x[i] & 0xff);
-    result[i] = ((x[i] >> 8) & 0xff);
-    result[i] = ((x[i] >>16) & 0xff);
-    result[i] = ((x[i] >> 24) & 0xff);
+  for(int i = 0; i < 64; i +=4){
+    result[i]		  = (x[i/4] & 0xff);
+    result[i+1] = ((x[i/4] >> 8) & 0xff);
+    result[i+2] = ((x[i/4] >>16) & 0xff);
+    result[i+3] = ((x[i/4] >> 24) & 0xff);
   }
-
-  //debug
-  cout<<"逆リトルエンディアン終了"<<endl;
 
 //16進数のstring型に変換する。これがkey_steam。
+string key_stream;
   for(int i = 0; i < 64; i++){
-    cout<<hex<<result[i];
+    stringstream ss;
+		ss << hex << result[i];
+		key_stream += ss.str();
   }
-  cout<<endl;
+  return key_stream;
 }
 
 int main(int argc, char const *argv[]) {
-  string key, nonce;
-  cout << "key?" << endl;
-  cin >> key;
-  cout << "nonce" << endl;
-  cin >> nonce;
-  chacha(key, nonce);
+  string key="0000000000000000000000000000000000000000000000000000000000000000", nonce="0000000000000000";
+	string key_stream;
+	long long int count = 0;
+	vector<string> mini_key;
+	bool flag = 0;	//mini_key作成のループ抜ける用
+	//4wordsのmini_keyを作成。これを16個あつめてkeyを作る(予定)
+	while(1){
+		for(int i = 0; i < 16; i++){
+			for(int j = 0; j < 16; j++){
+				for(int k = 0; k < 16; k++){
+					for(int l = 0; l < 16; l++){
+						// cout<<"i,j,k,l="<<i<<","<<j<<","<<k<<","<<l<<","<<"mini_key="<<key_generate(i , j , k ,l)<<endl;
+						mini_key.push_back(key_generate(i , j , k ,l)) ;
+						if(i == 15 && j == 15 && k == 15 && l == 15)	flag = 1;
+					}
+				}
+			}
+		}
+		if(flag == 1)	break;
+	}
+	for(int i = 0;i < 10; i++){
+		for(int j = 0; j < 4; j++){
+			key[j] = mini_key[i][j];
+		}
+		key_stream = chacha(key, nonce);
+		cout<<key_stream<<endl;
+		count++;
+	}
+	cout<<dec<<count<<endl;
   return 0;
 }
