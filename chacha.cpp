ebf67ac88fe412e43f6622cdfa3f1e7f8d0d7030
collagePC <ateruimashin2go@gmail.com>
2020-01-09 15:18:15 +0900

    書き直し

--- chacha.cpp
+++ chacha.cpp
@@ -2,7 +2,10 @@
 #include <string>
 #include <array>
 #include <cstring>
-
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <random>
 using namespace std;
 
 //出典:https://boringssl.googlesource.com/boringssl/+/master/crypto/chacha/chacha.c
@@ -21,7 +24,7 @@ uint32_t plus32(uint32_t x,uint32_t y){
 array<uint32_t,32> make_array_key(string s){
   array<uint32_t,32> key;
   int count = 0;
-  for(int i=0;i<s.size();i+=2){
+  for(int i=0;i < s.size();i+=2){
     string tmp;
     tmp += s[i];
     tmp += s[i+1];
@@ -35,7 +38,7 @@ array<uint32_t,32> make_array_key(string s){
 array<uint32_t,8> make_array_nonce(string s){
   array<uint32_t,8> key;
   int count = 0;
-  for(int i=0;i<s.size();i+=2){
+  for(int i=0;i < s.size();i+=2){
     string tmp;
     tmp += s[i];
     tmp += s[i+1];
@@ -46,11 +49,52 @@ array<uint32_t,8> make_array_nonce(string s){
   return key;
 }
 
+string key_generate(int a, int b, int c, int d){
+	string sub_key;
+	int bc[4] = {a, b, c, d};
+	for(int i = 0; i < 4; i++){
+		char c;
+		if(bc[i] < 10){
+			c = bc[i] + '0';
+		}else{
+			if(bc[i] == 10)	c = 'a';
+			if(bc[i] == 11)	c = 'b';
+			if(bc[i] == 12)	c = 'b';
+			if(bc[i] == 13)	c = 'd';
+			if(bc[i] == 14)	c = 'e';
+			if(bc[i] == 15)	c = 'f';
+		}
+		sub_key.push_back(c);
+	}
+	return sub_key;
+}
 
-void chacha(string key, string nonce) {
+//次のkeyとnonceを作成する。
+int dice(int i){
+  mt19937 mt;
+  random_device rnd;
+  mt.seed(rnd());
+  uniform_int_distribution<> rand1(0, i);
+  return rand1(mt);
+}
 
-// //debug
-// cout<<"key="<<key<<"&nonce="<<nonce<<endl;
+string next_key(string key){
+  string n_key;
+  for(int i = 0; i< 64; i++){
+    n_key.push_back(key[i]);
+  }
+  return n_key;
+}
+
+string next_nonce(string key){
+  string n_nonce;
+  for(int i = 0; i < 16; i++){
+    n_nonce.push_back(key[i+110]);
+  }
+  return n_nonce;
+}
+
+string chacha(string key, string nonce) {
 
   //Initial Stateを作成する。
   array<uint32_t,64>  in = {101, 120, 112, 97,
@@ -63,67 +107,26 @@ void chacha(string key, string nonce) {
   array<uint32_t, 8> block_count = {0, 0, 0, 0, 0, 0, 0, 0};
   array<uint32_t, 8> n(make_array_nonce(nonce));
 
-	// //debug
-	// cout<<"key"<<endl;
-	// for(int i=0;i<32;i++){
-	// 	cout<<"i="<<i<<" key="<<k[i]<<endl;
-	// }
-	// cout<<"block_count"<<endl;
-	// for(int i=0;i<8;i++){
-	// 	cout<<"i="<<i<<" count="<<block_count[i]<<endl;
-	// }
-	// for(int i=0;i<8;i++){
-	// 	cout<<"i="<<i<<" nonce="<<nonce[i]<<endl;
-	// }
 
-
-  //debug
-  // cout<<"key作成終了"<<endl;
-
-  //上で作成したkey,block_count,nonceをInital Stateに代入。
-  //この時、Initial Stateは64要素ある。
   for(int i = 16; i < 64; i++){
-		//debug
-		// cout<<"i="<<i<<endl;
     if(i >= 16 && i < 48){
       in[i] = k[i - 16];
-      //debug
-      // cout<<1<<endl;
     }else if(i >= 48 && i < 56){
       in[i] = block_count[i - 48];
-      //debug
-      // cout<<2<<endl;
     }else{
       in[i] = n[i - 56];
-      //debug
-      // cout<<3<<endl;
     }
   }
 
-//debug
-// cout<<"1*64のInital State完成"<<endl;
-// for(int i=0;i<64;i++){
-// 	cout<<i<<"="<<in[i]<<endl;
-// }
-
 /*4*4のInital Stateに変換する。64要素あるInital Stateを4要素ずつ取り出し、リトルエンディアンに変換して4*4行列に代入する。
-この時、4*4行列ではなく、QRの計算のために1*16行列にする。つまり、このあと計算するのはIn[]ではなく、x[]である。
-また、これはリトルエンディアンにしている。*/
+この時、4*4行列ではなく、QRの計算のために1*16行列にする。つまり、このあと計算するのはIn[]ではなく、x[]である。*/
   uint32_t x[16]={};
   for(int i = 0; i < 64; i+=4){
     x[i / 4] = in[i] | (in[i+1] << 8) | (in[i+2] << 16) | (in[i+3] << 24);
-		// //debug
-		// cout<<i/4<<"="<<x[i/4]<<endl;
   }
-
-  //debug
-  // cout<<"4*4のInital State完成"<<endl;
 
   //QR前のx[]をコピーする。
   uint32_t cp[16]={};
-
-	//debug
-	// cout<<"cp作成"<<endl;
 
   for(int i = 0; i < 16; i++){
 		cp[i] = x[i];
@@ -143,54 +146,68 @@ void chacha(string key, string nonce) {
 		QR(x[3], x[4], x[ 9], x[14]); // diagonal 4
   }
 
-  //debug
-  // cout<<"QR終了"<<endl;
-
   //QR前x[]とQR後x[]を2^32を法とする加算を行う。
   for(int i = 0; i < 16; i++){
     x[i] = plus32(x[i], cp[i]);
   }
 
-	for(int i=0;i<16;i++){
-		cout<<i<<"="<<x[i]<<endl;
-	}
-
-  //debug
-  // cout<<"2^32加算終了"<<endl;
 
   //リトルエンディアンを逆変換する
   uint32_t result[64]={};
   for(int i = 0; i < 64; i +=4){
-    result[i]		  = (x[i/4] & 0xff);
-		cout<<i<<"="<<result[i]<<endl;
-
-    result[i+1] = ((x[i/4] >> 8) & 0xff);
-		cout<<i+1<<"="<<result[i+1]<<endl;
-
-    result[i+2] = ((x[i/4] >>16) & 0xff);
-		cout<<i+2<<"="<<result[i+2]<<endl;
-
+    result[i]		= (x[i/4] & 0xff);
+    result[i+1] = ((x[i/4] >>  8) & 0xff);
+    result[i+2] = ((x[i/4] >> 16) & 0xff);
     result[i+3] = ((x[i/4] >> 24) & 0xff);
-		cout<<i+3<<"="<<result[i+3]<<endl;
   }
-
-  //debug
-  // cout<<"逆リトルエンディアン終了"<<endl;
 
 //16進数のstring型に変換する。これがkey_steam。
+	string key_stream;
   for(int i = 0; i < 64; i++){
-    cout<<hex<<result[i];
-		if((i+1)%4 ==0){
-			cout<<"  ";
+    stringstream ss;
+		ss << hex << result[i];
+
+		if(ss.str().size() != 2 ){
+			string s = '0'+ss.str();
+			key_stream += s;
+		}else{
+			key_stream += ss.str();
 		}
   }
-  cout<<endl;
+  return key_stream;
 }
 
 int main(int argc, char const *argv[]) {
-  string key, nonce,cipher;
-	key = "23AD52B15FA7EBDC4672D72289253D95DC9A4324FC369F593FDCC7733AD77617";
-  nonce = "5A5F6C13C1F12653";
-  chacha(key, nonce);
+  string key, nonce;
+	string key_stream;
+
+  cout<<"keyの初期値(0を入れると0~0になります)"<<endl;
+  cin>>key;
+  if(key == "0")  key = "0000000000000000000000000000000000000000000000000000000000000000";
+  cout<<"nonceの初期値(0を入れると0~0になります)"<<endl;
+  cin>>nonce;
+  if(nonce == "0")  nonce = "0000000000000000";
+
+	cout<<"Writing...Please wait..."<<endl;	//実行中何も表示されないと寂しいので
+
+	//keyを作成して、chacha関数からkey_streamを受け取り、ファイルに出力する。
+	for(int i = 0;i < 65536; i++){
+		key_stream = chacha(key, nonce);
+
+		//ファイル出力
+		string filename = "key_stream_result.txt";
+		ofstream	writing_file;
+		writing_file.open(filename, ios::app);
+		writing_file << key_stream << endl;
+
+    string second_key = next_key(key_stream);
+    string second_nonce = next_nonce(key_stream);
+    key = second_key;
+    nonce = second_nonce;
+
+		if(i % 1000 == 0)	cout<<"Now,count is "<< i <<endl;	//暇つぶし
+
+	}
+  cout << "End of generating key stream" << endl;
   return 0;
 }
